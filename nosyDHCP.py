4fbf5e7fa9ed489200e50da37f66fa6aaaa4ba3f
Angelo <119819586+Brielon@users.noreply.github.com>
2024-04-19 17:52:10 -0300

    Update nosyDHCP.py
    adding function that actually changes the "offer" data

--- nosyDHCP.py
+++ nosyDHCP.py
@@ -1,5 +1,10 @@
 import socket
 import struct
+#ex:"192.168.1.**"
+TFTP_IP_ADDRS = "replace with tftp server address"
+#ex:"image.efi"
+TFTP_NAME_FILE = "replace with of the name of the file that the PXE client must download from the TFTP server"
+
 
 '''  the program captures the "offer" data that
      is coming from the dhcp server as a response 
@@ -29,3 +34,20 @@ with socket.socket(socket.AF_INET,socket.SOCK_DEGRAM) as returnerSocket:
   returnerSocket.sendto(("<broadcast>",68))
 
 #pxe machine configured is easy... now just configure a tftp server
+
+def addDataOptions(data):
+     option66 = struct.pack("!BBBBBB",66,4,*TFTP_IP_ADDRS.split("."))
+     option67 = struct.pack("!BB",67,len(TFTP_NAME_FILE))+TFTP_NAME_FILE.encode("ascii")
+          '''
+     the magic cookie is used to 
+     find where the dhcp options start
+     '''
+     #magic cookie == 0x63 0x82 0x53 0x63
+     magicCookie = b"\x63\x82\x53\x63"
+     newdata = b""
+     for byte in data:
+          newdata += struct.pack(!B,byte)
+          if newdata[-4:] == magicCookie:
+               newdata += option66
+               newdata += option67
+     return newdata
